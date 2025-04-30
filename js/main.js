743d69dd7286b9d71dfb901ba2f0d5cc62fdef41
hanol-gida <patience1703@gmail.com>
2025-04-30 23:36:00 +0900

    add animation

---
+++ js/main.js
@@ -0,0 +1,59 @@
+// 장바구니!
+// 장바구니 관련 요소 찾기.
+const basketStarterEl = document.querySelector('header .basket-starter')
+const basketEl = basketStarterEl.querySelector('.basket')
+
+basketStarterEl.addEventListener('click', event => {
+  event.stopPropagation() // 이벤트 버블링 정지! - 버튼을 클릭했을 때 드롭다운 메뉴가 나타나야 함.
+  basketEl.classList.contains('show') ? hideBasket() : showBasket()
+})
+basketEl.addEventListener('click', event => {
+  event.stopPropagation() // 이벤트 버블링 정지! - 드롭다운 메뉴 영역을 클릭했을 때 메뉴가 사라지지 않아야 함.
+})
+// 화면 전체를 클릭했을 때 메뉴가 사라짐.
+window.addEventListener('click', () => {
+  hideBasket()
+})
+
+// 특정 로직을 직관적인 함수 이름으로 묶음.
+function showBasket() {
+  basketEl.classList.add('show')
+}
+function hideBasket() {
+  basketEl.classList.remove('show')
+}
+
+// 검색!
+const headerEl = document.querySelector('header')
+const headerMenuEl = [...headerEl.querySelectorAll('ul.menu > li')]
+const searchWrapEl = headerEl.querySelector('.search-wrap')
+const searchStarterEl = headerEl.querySelector('.search-starter')
+const searchCloserEl = searchWrapEl.querySelector('.search-closer')
+const searchShadowEl = searchWrapEl.querySelector('.shadow')
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]
+
+searchStarterEl.addEventListener('click', showSearch)
+searchCloserEl.addEventListener('click', hideSearch)
+searchShadowEl.addEventListener('click', hideSearch)
+
+function showSearch() {
+  headerEl.classList.add('searching')
+  document.documentElement.classList.add('fixed')
+  headerMenuEl.reverse().forEach((el, index) => {
+    el.style.transitionDelay = index * .4 / headerMenuEl.length + 's'
+  })
+  searchDelayEls.forEach((el, index) => {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+  })
+}
+function hideSearch() {
+  headerEl.classList.remove('searching')
+  document.documentElement.classList.remove('fixed')
+  headerMenuEl.reverse().forEach((el, index) => {
+    el.style.transitionDelay = index * .4 / headerMenuEl.length + 's'
+  })
+  searchDelayEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+  })
+  searchDelayEls.reverse()
+}
